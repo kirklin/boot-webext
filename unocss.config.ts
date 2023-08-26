7cd4d95f7e75b66a8e920cd4f2ade8b5c0b8f339
Kirk Lin <linkirk@163.com>
2023-08-26 16:47:40 +0800

    feat: add `unocss-preset-ease`, `unocss-preset-chinese`

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "unocss/vite";
 import { presetAttributify, presetIcons, presetUno, transformerDirectives } from "unocss";
+import presetChinese from "unocss-preset-chinese";
+import presetEase from "unocss-preset-ease";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
+    presetChinese(),
+    presetEase(),
     presetIcons(),
   ],
   transformers: [

60c3eb021d805b0b7e9c42244a3f202bbedd0bc0
Kirk Lin <linkirk@163.com>
2023-08-26 15:00:22 +0800

    chore: clear all files

--- src/ui/options.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import Options from "@/ui/Options.vue";
-import router from "@/ui/router";
-import store from "@/ui/store";
-
-createApp(Options).use(store).use(router).mount("#app");
--- src/ui/popup.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import Popup from "@/ui/Popup.vue";
-import router from "@/ui/router";
-import store from "@/ui/store";
-
-createApp(Popup).use(store).use(router).mount("#app");
